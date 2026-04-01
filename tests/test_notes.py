34e4212721485a94546a0aba6dcf596acec80fd5
alexgeleisha <121766149+alexgeleisha@users.noreply.github.com>
2026-04-01 20:12:00 +0300

    Add delete test

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -3,3 +3,8 @@ from api.notes import create_note, list_notes
 def test_create_note():
     create_note("Test")
     assert len(list_notes()) > 0
+from api.notes import update_note
+
+def test_update():
+    update_note(0, "Updated")
+    assert True
